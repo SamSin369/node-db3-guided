74da1095623840823561f0458da7cc9d7e6fbbd1
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-05-12 18:37:48 +0200

    wip

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -4,14 +4,14 @@ const db = require("../../data/db-config.js");
 
 const router = express.Router();
 
+
+function getUsersWithPosts() {
+  
+}
+
+
 router.get("/", (req, res) => {
-  db("users")
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => {
-      res.status(500).json({ message: "Failed to get users" });
-    });
+
 });
 
 router.get("/:id", (req, res) => {
